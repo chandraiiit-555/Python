a17817ec9a6e1e5c6b4c3c4e249033db50698202
chandraiiit-555 <58101680+chandraiiit-555@users.noreply.github.com>
2020-02-17 21:09:22 +0530

    Update create_strings.py

--- String/create_strings.py
+++ String/create_strings.py
@@ -13,6 +13,7 @@
 
 	there is no char data type in python so single character we can assume as string with length 1
 
+
 '''
 
 '''
@@ -22,16 +23,17 @@
 
 '''
 string1_with_single_quote = 'This is chandra tanikonda'
-print("strings creation with single quotes")
+print('\"strings creation with single quotes\"')
 print(string1_with_single_quote)
 print("\n")
 
 strings_with_double_quote = "Jama Investment Advisory"
-print("Strings with double quotes")
+print("\'Strings with double quotes\'")
 print(strings_with_double_quote)
 print("\n")
 
 strings_with_thriple_quotes = "Mutual Fund and Direct Equity Advisory Services"
-print("Strings with thriple quotes")
+
+print('''Strings with thriple quotes''')
 print(strings_with_thriple_quotes)
 print("\n")
